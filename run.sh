23f49cb428b42aeaa40ccc2abccf108d45f2e6a8
FaceStark <mryangfor@yeah.net>
2021-12-01 21:38:33 +0800

    V1

--- run.sh
+++
@@ -1,5 +0,0 @@
-if [ ! -d models/paddle ]; then
-    x2paddle --framework=caffe --prototxt=models/face_mask_detection.prototxt --weight=models/face_mask_detection.caffemodel --save_dir=./ --params_merge
-    mv inference_model models/paddle
-fi
-python3 paddle_infer.py
